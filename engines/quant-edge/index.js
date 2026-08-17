d0b6a22d13f1cec47e1b1ae003fa24588790e4ab
rasheadsca-star <rasheadsca@gmail.com>
2026-08-17 09:59:54 +0300

    Expose hardened QUANT EDGE data boundary

--- engines/quant-edge/index.js
+++ engines/quant-edge/index.js
@@ -1,11 +1,9 @@
 'use strict';
-
-const config = require('./config');
-const engine = require('./engine');
-const broker = require('./broker-intelligence');
-const probability = require('./probability');
-const tracking = require('./tracking');
-const core = require('./core');
-const dataBoundary = require('./data-boundary');
-
-module.exports = { config, ...engine, broker, probability, tracking, core, dataBoundary };
+const config=require('./config');
+const engine=require('./engine');
+const broker=require('./broker-intelligence');
+const probability=require('./probability');
+const tracking=require('./tracking');
+const core=require('./core');
+const boundary=require('./data-boundary');
+module.exports={config,...engine,broker,probability,tracking,core,boundary,dataBoundary:boundary};
