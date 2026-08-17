f5568ad6d0e59601d12ec1ea21b4d7dd473ec0fc
rasheadsca-star <rasheadsca@gmail.com>
2026-08-17 09:35:56 +0300

    feat(quant-edge): expose isolated engine API

---
+++ engines/quant-edge/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const config = require('./config');
+const engine = require('./engine');
+const broker = require('./broker-intelligence');
+const probability = require('./probability');
+const tracking = require('./tracking');
+const core = require('./core');
+
+module.exports = { config, ...engine, broker, probability, tracking, core };
