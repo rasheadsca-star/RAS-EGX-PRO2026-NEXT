7f4d0c8242bac3696c192f7c9fa92f2981127f2c
rasheadsca-star <rasheadsca@gmail.com>
2026-08-23 20:29:09 +0300

    Load automatic fundamentals in RC2 portfolio module

--- tfe-v20/public/portfolio-manager.js
+++ tfe-v20/public/portfolio-manager.js
@@ -1,2 +1,3 @@
-export * from './portfolio-manager-core.js?v=20260823-deep1';
-import './portfolio-deep-analysis.js?v=20260823-deep1';
+export * from './portfolio-manager-core.js?v=20260823-deep2';
+import './portfolio-deep-analysis.js?v=20260823-deep2';
+import './fundamental-auto.js?v=20260823-fund1';
