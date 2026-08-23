f9316ae73812456c15a72f27ebbb7502870f38bd
rasheadsca-star <rasheadsca@gmail.com>
2026-08-23 20:36:03 +0300

    Guard automatic fundamentals to browser runtime

--- tfe-v20/public/portfolio-manager.js
+++ tfe-v20/public/portfolio-manager.js
@@ -1,3 +1,3 @@
 export * from './portfolio-manager-core.js?v=20260823-deep2';
 import './portfolio-deep-analysis.js?v=20260823-deep2';
-import './fundamental-auto.js?v=20260823-fund1';
+if (typeof document !== 'undefined') import('./fundamental-auto.js?v=20260823-fund2');
