1432127c80e938d026e33db4ba9d9a75aefe22ea
rasheadsca-star <rasheadsca@gmail.com>
2026-08-24 22:27:36 +0300

    Load complete RC2 recommendation history UI

--- tfe-v20/public/portfolio-manager.js
+++ tfe-v20/public/portfolio-manager.js
@@ -3,4 +3,5 @@ import './portfolio-deep-analysis.js?v=20260823-deep2';
 if (typeof document !== 'undefined') {
   import('./fundamental-auto.js?v=20260823-fund2');
   import('./rc2-ops-center.js?v=20260824-opsall1');
+  import('./recommendation-history-ui.js?v=20260824-history1');
 }
