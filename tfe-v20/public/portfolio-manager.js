a596f76464abd507580ae162b4036ca74236a5d2
rasheadsca-star <rasheadsca@gmail.com>
2026-08-24 20:13:56 +0300

    Load RC2 operations center from V16.9 addon entry point

--- tfe-v20/public/portfolio-manager.js
+++ tfe-v20/public/portfolio-manager.js
@@ -1,3 +1,6 @@
 export * from './portfolio-manager-core.js?v=20260823-deep2';
 import './portfolio-deep-analysis.js?v=20260823-deep2';
-if (typeof document !== 'undefined') import('./fundamental-auto.js?v=20260823-fund2');
+if (typeof document !== 'undefined') {
+  import('./fundamental-auto.js?v=20260823-fund2');
+  import('./rc2-ops-center.js?v=20260824-opsall1');
+}
