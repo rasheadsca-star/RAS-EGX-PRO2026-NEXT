348d3426b000e3635af73eab80d7638ecb4324aa
rasheadsca-star <rasheadsca@gmail.com>
2026-08-27 22:44:42 +0300

    Load RC2 snapshot date guard

--- tfe-v20/public/portfolio-manager.js
+++ tfe-v20/public/portfolio-manager.js
@@ -1,2 +1,3 @@
 export * from './portfolio-manager-core.js?v=20260823-deep1';
 import './portfolio-deep-analysis.js?v=20260823-deep1';
+import './snapshot-date-fix.js?v=20260827-snapshot-date-guard1';
